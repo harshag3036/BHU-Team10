f917d5a994d1ec16857b3e72f56a8adf768b0c3d
Subham <subhamkumarr456@gmail.com>
2021-08-28 03:10:08 -0400

    index.html modified

--- api/script.js
+++ api/script.js
@@ -1,9 +1,30 @@
-window.addEventListener('load', fetchStocks);
-
+document.addEventListener('load', fetchStocks);
+var historyList=[];
 async function fetchStocks() {
     const response = await fetch('http://localhost:3000/stock');
     const stocks = await response.json();
     // waits until the request completes...
-    console.log(stocks);
+    //console.log(stocks);
     
+  }
+
+function searchFunction() {
+    const val = document.getElementById("search").value;
+    console.log(val);
+    historyList.push(val);
+    document.getElementById("search").value="";
+    ul = document.getElementById("history_list");
+
+  document.getElementById("search_box").appendChild(ul);
+
+
+  let li = document.createElement('li');
+  ul.appendChild(li);
+
+  li.innerHTML += val;
+
+  }
+
+  function showFunction(){
+
   }
